9cec668844e91121104159adf1616d6464382c5e
BlossomV0 <roberto.partidag@gmail.com>
2025-02-28 00:47:28 -0600

    Correción Main

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Ventana ventana = new Ventana();
-            ventana.setVisible(true);
-        });
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
     }
 }
